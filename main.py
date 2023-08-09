2c676074fa1cdfc49e2a40ea4a9ba5a95157e795
Martin Biroščák <mbiroscak@pttsoftware.eu>
2023-08-09 09:52:31 +0200

    Refactor and Improve Readability of main.py
    - Refactor streaming state handling by introducing functions:
      - wait_message_streaming_visible
      - wait_message_streaming_detached
      replacing the removed is_result_streaming_class_present.
    - Modify send_message_async function to use the new streaming wait functions.
    - Enhance readability by fixing spacing and indentation across the file.
    - Add extra line breaks for improved code organization.

--- main.py
+++ main.py
@@ -5,45 +5,60 @@ import fire
 import sys
 import re
 
-async def is_result_streaming_class_present(locator):
-        return await locator.locator(".result-streaming").wait_for(state="detached"),
 
 
-async def login_async(username,password):
+async def wait_message_streaming_visible(locator):
+    return await locator.locator(".result-streaming").wait_for(state="visible"),
+async def wait_message_streaming_detached(locator):
+    return await locator.locator(".result-streaming").wait_for(state="detached"),
+
+
+async def login_async(username, password):
     async with async_playwright() as p:
         browser = await p.chromium.connect_over_cdp("http://localhost:9222")
         default_context = browser.contexts[0]
-        page = default_context.pages[0]                
+        page = default_context.pages[0]
         await page.goto("http://chat.openai.com")
         await page.get_by_role("button", name="Log in").click()
         await page.get_by_role("textbox").fill(username)
         await page.get_by_role("button", name="Continue", exact=True).click()
         await page.get_by_role("textbox").fill(password)
         await page.get_by_role("button", name="Continue", exact=True).click()
-        await page.get_by_role('button',  name='Next').click();
-        await page.get_by_role('button',  name='Next').click();
+        await page.get_by_role('button', name='Next').click();
+        await page.get_by_role('button', name='Next').click();
         await page.get_by_role('button', name='Done').click();
-       	await page.get_by_role('button', name='Dismiss').click();
+        await page.get_by_role('button', name='Dismiss').click();
+
 
 async def new_chat_async():
     async with async_playwright() as p:
         browser = await p.chromium.connect_over_cdp("http://localhost:9222")
         default_context = browser.contexts[0]
-        page = default_context.pages[0]                
+        page = default_context.pages[0]
         all_div_elements = await page.locator('a').filter(has_text="New chat").first.click()
+
+
+async def open_assitant():
+    async with async_playwright() as p:
+        browser = await p.chromium.connect_over_cdp("http://localhost:9222")
+        default_context = browser.contexts[0]
+        page = default_context.pages[0]
+        await page.goto("http://chat.openai.com")
+
 
 async def switch_to_4():
     async with async_playwright() as p:
         browser = await p.chromium.connect_over_cdp("http://localhost:9222")
         default_context = browser.contexts[0]
-        page = default_context.pages[0]                
+        page = default_context.pages[0]
         await page.get_by_text("GPT-4").click()
+
 
 async def switch_to_3():
     async with async_playwright() as p:
         browser = await p.chromium.connect_over_cdp("http://localhost:9222")
         default_context = browser.contexts[0]
-        page = default_context.pages[0]                
+        page = default_context.pages[0]
         await page.get_by_text("GPT-3.5").click()
 
 
@@ -51,15 +66,18 @@ async def send_message_async(message):
     async with async_playwright() as p:
         browser = await p.chromium.connect_over_cdp("http://localhost:9222")
         default_context = browser.contexts[0]
-        page = default_context.pages[0]                
+        page = default_context.pages[0]
         await page.get_by_role("textbox").fill(message)
         await page.locator("form").get_by_role("button").last.click()
-        await is_result_streaming_class_present(page.locator("main .group"))
+        await wait_message_streaming_visible(page.locator("main .group"))
+        await wait_message_streaming_detached(page.locator("main .group"))
         i = await page.locator("main .group").count()
-        print(await page.locator("main .group").nth(i-2).inner_text())
+        print(await page.locator("main .group").filter(has_not_text="?").last.inner_text())
+
 
 def new_chat():
     asyncio.run(new_chat_async())
+
 
 def send_message(message=None):
     if message is None:
@@ -67,14 +85,17 @@ def send_message(message=None):
         message = sys.stdin.read().strip()
     asyncio.run(send_message_async(message))
 
-def login(username,password):
-   asyncio.run(login_async(username,password))
+
+def login(username, password):
+    asyncio.run(login_async(username, password))
+
 
 if __name__ == '__main__':
-        fire.Fire({
-            'm': send_message,
-            'l': login,
-            'n' : new_chat,
-            "3" : switch_to_3,	
-            "4" : switch_to_4
-        })
+    fire.Fire({
+        'm': send_message,
+        'l': login,
+        'o': open_assitant,
+        'n': new_chat,
+        "3": switch_to_3,
+        "4": switch_to_4
+    })
